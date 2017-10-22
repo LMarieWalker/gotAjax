72b33247afd034985e53e15fcc0dd2eb7f12ecf8
Rod <LourdesMarieWalker@gmail.com>
2017-10-21 21:33:38 -0700

    changes

--- script.js
+++ script.js
@@ -1,96 +1,120 @@
-// https://www.anapioficeandfire.com/api/characters
 
 $(document).ready(function(){
 
-  var notFound = document.createElement("div");
-  notFound.className = "notFound";
+  var notFound = document.getElementById("notFound");
 
-  // CHARACTERS
-  var charArr = ['Daenerys Targaryen', 'Arya Stark', 'Jon Snow', 'Marie Walker', 'Armon Walker', 'Jacqueline Marie', 'Nestor Luis'];
+  // CHARACTERS: LOOP
+  var charDiv = document.getElementById("mainCharCard");
+  var charArr = ['Daenerys Targaryen', 'Arya Stark', 'Jon Snow', 'Marie Walker'];
 
-  var charDiv = document.createElement("div");
-  charDiv.className = "mainCard";
+  function renderCharPage() {
+    for (let i = 0; i < charArr.length; i++) {
+      $.get(('https://www.anapioficeandfire.com/api/characters?name='+charArr[i]), function(charData){
+        if (charData == 0) {
+          console.log('Character '+charArr[i]+' not found');
 
-  for (let i = 0; i < charArr.length; i++) {
-    $.get(('https://www.anapioficeandfire.com/api/characters?name='+charArr[i]), function(charData){
-      if (charData == 0) {
-        console.log('Character '+charArr[i]+' not found');
+          let nfh3 = document.createElement('h4');
+          nfh3.innerHTML = ('Character '+charArr[i]+' not found');
+          notFound.append(nfh3);
 
-        let nfh3 = document.createElement('h4');
-        nfh3.innerHTML = ('Character '+charArr[i]+' not found');
-        notFound.append(nfh3);
+        } else {
+          console.log(charData);
 
-      } else {
-        console.log(charData);
+          let myDiv = document.createElement("div");
+          myDiv.className="cards";
 
-        let myDiv = document.createElement("div");
-        myDiv.className="cards";
+          let nameHeader = document.createElement("h3");
+          nameHeader.innerHTML = ("Character: "+charArr[i]);
+          myDiv.append(nameHeader);
 
-        let nameHeader = document.createElement("h3");
-        nameHeader.innerHTML = ("Character: "+charArr[i]);
-        myDiv.append(nameHeader);
+          let charTitle = document.createElement('p');
+          charTitle.innerHTML = ("Title: "+charData[charData.length-1].titles); // TODO - add loop for titles
+          myDiv.append(charTitle);
 
-        let charTitle = document.createElement('p');
-        charTitle.innerHTML = ("Title: "+charData[charData.length-1].titles); // TODO - add loop for titles
-        myDiv.append(charTitle);
+          let spouse = document.createElement('p');
+            $.get((charData[charData.length-1].spouse), function(theSpouse){
+              spouse.innerHTML = ("Spouse: "+theSpouse.name);
+              myDiv.append(spouse);
+            })
 
-        let spouse = document.createElement('p');
-          $.get((charData[charData.length-1].spouse), function(theSpouse){
-            spouse.innerHTML = ("Spouse: "+theSpouse.name);
-            myDiv.append(spouse);
-          })
+          charDiv.append(myDiv);
+        }
 
-        charDiv.append(myDiv);
-      }
-
-    document.body.append(charDiv);
-    document.body.append(notFound);
-
-    })
+      })
+    }
   }
 
-  // HOUSES
-  var houseArr = ['House Algood', 'House Ashford of Ashford', 'House Baelish of Harrenhal', 'House Stark', 'House Walker'];
 
-  var houseDiv = document.createElement("div");
-  houseDiv.className="mainCard"
+  // HOUSES: LOOP
+  var houseDiv = document.getElementById("mainHouseCard");
+  var houseArr = ['House Algood', 'House Ashford of Ashford', 'House Baelish of Harrenhal', 'House Stark'];
 
-  for (let i = 0; i < houseArr.length; i++) {
-    $.get(('https://www.anapioficeandfire.com/api/houses?name='+houseArr[i]), function(houseData){
-      if (houseData == 0) {
-        console.log('House '+houseArr[i]+' not found');
+  function renderHousesPage() {
+    for (let i = 0; i < houseArr.length; i++) {
+      $.get(('https://www.anapioficeandfire.com/api/houses?name='+houseArr[i]), function(houseData){
+        if (houseData == 0) {
+          console.log('House '+houseArr[i]+' not found');
 
-        let nfh3 = document.createElement('h4');
-        nfh3.innerHTML = (houseArr[i]+' not found');
-        notFound.append(nfh3);
+          let nfh3 = document.createElement('h4');
+          nfh3.innerHTML = (houseArr[i]+' not found');
+          notFound.append(nfh3);
 
-      } else {
-        console.log(houseData);
+        } else {
+          console.log(houseData);
 
-        let myHouseDiv = document.createElement("div");
-        myHouseDiv.className="cards";
+          let myHouseDiv = document.createElement("div");
+          myHouseDiv.className="cards";
 
-        let houseHeader = document.createElement("h3");
-        houseHeader.innerHTML = ("House: "+houseArr[i]);
-        myHouseDiv.append(houseHeader);
+          let houseHeader = document.createElement("h3");
+          houseHeader.innerHTML = ("House: "+houseArr[i]);
+          myHouseDiv.append(houseHeader);
 
-        let houseRegion = document.createElement('p');
-        houseRegion.innerHTML = ("Region: "+houseData[houseData.length-1].region);
-        myHouseDiv.append(houseRegion);
+          let houseRegion = document.createElement('p');
+          houseRegion.innerHTML = ("Region: "+houseData[houseData.length-1].region);
+          myHouseDiv.append(houseRegion);
 
-        let leader = document.createElement('button');
+          let leader = document.createElement('button');
+            $.get((houseData[houseData.length-1].currentLord), function(theLeader){
+              leader.innerHTML = ("Leader: "+theLeader.name);
+              myHouseDiv.append(leader);
+            })
 
-          $.get((houseData[houseData.length-1].currentLord), function(theLeader){
-            leader.innerHTML = ("Leader: "+theLeader.name);
-            myHouseDiv.append(leader);
-          })
+          houseDiv.append(myHouseDiv);
+        }
 
-        houseDiv.append(myHouseDiv);
-      }
-
-    document.body.append(houseDiv);
-    document.body.append(notFound);
-
-    })
+      })
+    }
   }
+
+
+  renderCharPage();
+  renderHousesPage();
+
+  function resetCharacters() {
+    chars = [];
+    document.getElementsByClassName('className')
+  }
+
+
+  // EVENT HANDLERS
+  $("#searchButton").click(function(){
+     let user_provided_id = $("#searchInput").val();
+     if(user_provided_id){
+       renderPage({single: user_provided_id});
+     }
+  })
+
+  $("#clearButton").click(function(){
+    $("#searchInput").val("");
+    resetCharacters();
+    renderPage();
+  })
+
+  $(document).on('click', '.heir_link', function(e){
+    let my_id = e.target.id;
+    renderPage({add:my_id});
+    e.preventDefault();
+  })
+
+
 })
